1d172fdf133c53b5da65b3baa91b3282e9c04de3
lanheiv <markussmpp@gmail.com>
2025-04-16 13:16:27 +0300

    add migrate

--- database/migrations/2025_04_16_095609_subjects.php
+++ database/migrations/2025_04_16_095609_subjects.php
@@ -6,22 +6,15 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tests', function (Blueprint $table) {
             $table->id();
             $table->string('subject_name');
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('tests');
     }
 };
--- database/migrations/2025_04_16_095808_grades.php
+++ database/migrations/2025_04_16_095808_grades.php
@@ -6,19 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        //
+        Schema::create('grades', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('subject_id');
+            $table->unsignedTinyInteger('grade');
+            $table->timestamp('graded_at')->useCurrent();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('grades');
     }
 };
